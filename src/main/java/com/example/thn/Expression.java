6d719cbf49e9f8810672843570775fd3bb817bf5
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-07-16 17:36:28 -0400

    tdd by example

---
+++ src/main/java/com/example/thn/Expression.java
@@ -0,0 +1,9 @@
+package com.example.thn;
+
+public interface Expression {
+    Money reduce(Bank bank, String to);
+
+    Expression plus(Expression addend);
+
+    Expression times(int multiplier);
+}
---
+++ src/main/java/com/example/thn/Money.java
@@ -0,0 +1,52 @@
+package com.example.thn;
+
+public class Money implements Expression {
+    final int amount;
+    private final String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    String currency() {
+        return currency;
+    }
+
+    static Money dollar(int amount){
+        return new Money(amount, "USD");
+    }
+
+    static Money franc(int amount){
+        return new Money(amount, "CHF");
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+                && this.currency == money.currency;
+    }
+
+    @Override
+    public Money reduce(Bank bank, String to){
+        return new Money(amount / bank.rate(this.currency, to), to);
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    @Override
+    public Expression times(int multiplier) {
+        return new Money(amount * multiplier, this.currency);
+    }
+
+    @Override
+    public Expression plus(Expression addend){
+        return new Sum(this, addend);
+    }
+}
